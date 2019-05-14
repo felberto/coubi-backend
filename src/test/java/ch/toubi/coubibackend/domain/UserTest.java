408949a898bc9e74653b9be8dbb90b5d103c3d7f
felberto <tobias.felber97@gmail.com>
2019-05-14 07:44:04 +0200

    added test

---
+++ src/test/java/ch/toubi/coubibackend/domain/UserTest.java
@@ -0,0 +1,54 @@
+package ch.toubi.coubibackend.domain;
+
+import org.junit.Test;
+
+public class UserTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void getFirstName() {
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void setUsername() {
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+
+    @Test
+    public void equals1() {
+    }
+
+    @Test
+    public void canEqual() {
+    }
+
+    @Test
+    public void hashCode1() {
+    }
+
+    @Test
+    public void toString1() {
+    }
+}
